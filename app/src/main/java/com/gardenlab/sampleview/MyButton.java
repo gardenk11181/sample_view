86c5ed4b1a0267e7a3c7182cb79f0649cf5f5d00
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-26 01:00:09 +0900

    MyButton added

---
+++ app/src/main/java/com/gardenlab/sampleview/MyButton.java
@@ -0,0 +1,62 @@
+package com.gardenlab.sampleview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import androidx.appcompat.widget.AppCompatButton;
+
+public class MyButton extends AppCompatButton {
+    final String TAG = "MyButton";
+    public MyButton(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public void init(Context context) {
+        setBackgroundColor(Color.CYAN);
+        setTextColor(Color.BLACK);
+
+        float textSize = getResources().getDimension(R.dimen.text_size);
+        setTextSize(textSize);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d(TAG, "onDraw: called");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: called");
+
+        int action = event.getAction();
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                setBackgroundColor(Color.BLUE);
+                setTextColor(Color.RED);
+                break;
+            case MotionEvent.ACTION_OUTSIDE:
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                setBackgroundColor(Color.CYAN);
+                setTextColor(Color.BLACK);
+
+                break;
+        }
+
+        invalidate();
+
+        return true;
+    }
+}
